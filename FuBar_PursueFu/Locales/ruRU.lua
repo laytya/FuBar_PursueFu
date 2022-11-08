43a5c7c8e8954bf455766cfcd06ff3666ddd5b61
shikulja <gamemazahist@gmail.com>
2022-11-08 20:38:35 +0300

    add RU loc

---
+++ FuBar_PursueFu/Locales/ruRU.lua
@@ -0,0 +1,36 @@
+AceLibrary("AceLocale-2.1"):RegisterTranslation("FuBar_PursueFu", "ruRU", function()
+	return {
+		
+		["chatCommands"] = nil,
+		
+		["Minimap"] = "Миникарта",
+		["Show Minimap Icon"] = "Показать значок миникарты",
+		["Show the minimap tracking icon."] = "Показать значок отслеживания на миникартке.",
+		["Right-click to switch abilites."] = "Щелкните правой кнопкой мыши для переключения способностей.",
+		
+		["Hint"] = "Совет",
+		["Show Tooltip Hint"] = "Показать совет в подсказке",
+		["Show the hint at the bottom of the FuBar tooltip."] = "Показать совет в нижней части всплывающей подсказки FuBar.",
+		
+		["Reminder"] = "Напоминание",
+		["Post-Mortem Reminder"] = "Посмертное напоминание",
+		["Remind me upon resurrection that my tracking has been deactivated."] = "Напомнить, что после воскрешения отслеживание деактивировалось.",
+		["Reminder: Due to your recent death, your trackers are no longer active."] = "Напоминание: В связи с вашей недавней смертью, ваши отслеживания больше не активны.",
+		
+		["Nothing to track."] = "Нечего отслеживать.",
+		["Tracking Off"] = "Отслеживание выключено.",
+		["No match!"] = "Не совпадает!",
+		
+		["Find Abilities"] = "Способности поиска",
+		["Sense Abilities"] = "Способности чутья",
+		["Track Abilities"] = "Способности отслеживания",
+		
+		["Herbs"] = "Травы",
+		["Mines"] = "Руды",
+		["Chests"] = "Сундуки",
+		["/db herbs"] = nil,
+		["/db mines"] = nil,
+		["/db chests"] = nil,
+		
+	}	
+end)
